33d7edc7f61e747fc3e76755ce423574b4dda29c
Blastonn <felipe.paulo07@hotmail.com>
2025-03-14 17:31:04 -0300

    Feat - Adicionar calculo de quadrados na area
    Adicionado calculo de quadrados para suprir toda
    a area do jogo.

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ const gridQuadrado = document.createElement("div");
 const quadrado = document.createElement("div");
 gridQuadrado.classList.add("grid");
 quadrado.classList.add("quadrado");
+const larguraArea = 600;
+const alturaArea = 600;
+const tamanhoQuadrado = 16;
+
+function calcularQuadrados(){
+    const quadradosPorLinha = Math.floor(larguraArea/tamanhoQuadrado);
+    const quadradosPorColuna = Math.floor(alturaArea/tamanhoQuadrado);
+    const valorTotal = quadradosPorLinha * quadradosPorColuna;
+    return valorTotal;
+}
+
+
+
+console.log(calcularQuadrados());
 
 container.appendChild(gridQuadrado);
 gridQuadrado.appendChild(quadrado);
+
 
 
 
