2435b938f01976707defd6b186d3b663a38923db
Blastonn <felipe.paulo07@hotmail.com>
2025-03-14 17:19:31 -0300

    feat- Adicionar area principal do jogo
    Adicionado area onde ficaram os quadrados
    16x16 do jogo.

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".container");
+
+const gridQuadrado = document.createElement("div");
+const quadrado = document.createElement("div");
+gridQuadrado.classList.add("grid");
+quadrado.classList.add("quadrado");
+
+container.appendChild(gridQuadrado);
+gridQuadrado.appendChild(quadrado);
+
+
+
+
