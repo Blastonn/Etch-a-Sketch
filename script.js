690d65b6f0d58809e2c4cd57b4957c3a3007bdb5
Blastonn <felipe.paulo07@hotmail.com>
2025-03-14 22:12:38 -0300

    Feat- Adicionar grade com os quadrados
    Adicionado os quadrados 16x16 nos quadrados.

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 const container = document.querySelector(".container");
 
 const gridQuadrado = document.createElement("div");
-const quadrado = document.createElement("div");
 gridQuadrado.classList.add("grid");
-quadrado.classList.add("quadrado");
+container.appendChild(gridQuadrado);
 const larguraArea = 600;
 const alturaArea = 600;
 const tamanhoQuadrado = 16;
 
-function calcularQuadrados(){
+function adicionarQuadrados(){
     const quadradosPorLinha = Math.floor(larguraArea/tamanhoQuadrado);
     const quadradosPorColuna = Math.floor(alturaArea/tamanhoQuadrado);
-    const valorTotal = quadradosPorLinha * quadradosPorColuna;
-    return valorTotal;
+
+
+    for(i=0; i < quadradosPorLinha; i++){
+        for(u= 0; u < quadradosPorColuna; u++){
+            const quadrado = document.createElement("div");
+            quadrado.classList.add("quadrado");
+            gridQuadrado.appendChild(quadrado);
+            quadrado.style.border = "solid 1x white";
+
+        }
+    }
 }
 
 
 
-console.log(calcularQuadrados());
-
-container.appendChild(gridQuadrado);
-gridQuadrado.appendChild(quadrado);
-
-
-
-
-
+adicionarQuadrados();
