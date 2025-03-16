d21243a62c0fa1c573ad7ef011311dae564bfa6b
Blastonn <felipe.paulo07@hotmail.com>
2025-03-16 15:13:16 -0300

    feat- Adicionar rgb nos quadrados com evento de mouse
    Foi adicionado evento para que os quadrados mudem de cor
    quando o mouse passa por cima.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const container = document.querySelector(".container");
-
 const gridQuadrado = document.createElement("div");
+
 gridQuadrado.classList.add("grid");
+
 container.appendChild(gridQuadrado);
-const larguraArea = 600;
-const alturaArea = 600;
-const tamanhoQuadrado = 16;
+const larguraArea = 960;
+const alturaArea = 960;
+const tamanhoQuadrado = 160;
+
+
+function randomColor(num){
+    return Math.floor(Math.random() * (num + 1)); 
+}
 
 function adicionarQuadrados(){
     const quadradosPorLinha = Math.floor(larguraArea/tamanhoQuadrado);
@@ -19,8 +25,18 @@ function adicionarQuadrados(){
             gridQuadrado.appendChild(quadrado);
         }
     }
+    pintarQuadrados();
 }
 
+function pintarQuadrados(){
+    const divQuadrados = document.querySelectorAll(".grid .quadrado");
+    divQuadrados.forEach(divQe => {
+        divQe.addEventListener("mouseover", () => {
+            const rnd = `rgb(${randomColor(255)}, ${randomColor(255)}, ${randomColor(255)})`;   
+            divQe.style.backgroundColor = rnd;
+     });
 
+});
+}
 
 adicionarQuadrados();
