bffb3b2945dc9af60057660b3e45b818005ff82f
Blastonn <felipe.paulo07@hotmail.com>
2025-03-17 13:59:43 -0300

    Feat - Adicionar mudanca de tela e opacidade
    Adicionado funcao de mudanda de tela e outra
    funcao que altera opacidade dos quadrados.

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ gridQuadrado.classList.add("grid");
 container.appendChild(gridQuadrado);
 const larguraArea = 960;
 const alturaArea = 960;
-const tamanhoQuadrado = 160;
+let quadradosPorLinha = 16;
+let quadradosPorColuna = 16;
 
 
 function randomColor(num){
@@ -14,29 +15,65 @@ function randomColor(num){
 }
 
 function adicionarQuadrados(){
-    const quadradosPorLinha = Math.floor(larguraArea/tamanhoQuadrado);
-    const quadradosPorColuna = Math.floor(alturaArea/tamanhoQuadrado);
+    gridQuadrado.innerText = "";
+    tamanhoQuadrado = larguraArea / quadradosPorLinha;
 
 
     for(i=0; i < quadradosPorLinha; i++){
         for(u= 0; u < quadradosPorColuna; u++){
             const quadrado = document.createElement("div");
             quadrado.classList.add("quadrado");
+            console.log(quadrado.style.width = `${tamanhoQuadrado}px`);
+            quadrado.style.height = `${tamanhoQuadrado}px`;
             gridQuadrado.appendChild(quadrado);
         }
     }
     pintarQuadrados();
+    opacidadeQuadrados();
 }
 
 function pintarQuadrados(){
     const divQuadrados = document.querySelectorAll(".grid .quadrado");
     divQuadrados.forEach(divQe => {
-        divQe.addEventListener("mouseover", () => {
-            const rnd = `rgb(${randomColor(255)}, ${randomColor(255)}, ${randomColor(255)})`;   
-            divQe.style.backgroundColor = rnd;
+        divQe.addEventListener("mouseover", (e) => {
+            const rnd = `rgb(${randomColor(255)}, ${randomColor(255)}, ${randomColor(255)})`;
+            divQe.style.backgroundColor = "pink";
      });
-
 });
 }
+function opacidadeQuadrados(){
+    const divQuadrados = document.querySelectorAll(".grid .quadrado");
+    divQuadrados.forEach(divQe => {
+        divQe.addEventListener("mouseout", (e) => {
+            let opacidadeatual = parseFloat(window.getComputedStyle(divQe).opacity);
 
+            divQe.style.opacity = opacidadeatual + 0.30;
+   
+     });
+});
+
+}
+
+
+const button = document.querySelector(".btn");
+
+
+function mudarTela(){
+    button.addEventListener("click", () =>{
+        let valorUsu = parseInt(window.prompt("Digite o tamanho que deseja de um a cem: ", "0"));
+
+       if(valorUsu >= 1 && valorUsu < 100 ){
+        
+        quadradosPorLinha = valorUsu;
+        quadradosPorColuna = valorUsu;
+
+    
+        adicionarQuadrados();
+       }else{
+        alert('Digite algo novamente entre 1 e 100');
+       }
+    });
+}
+
+mudarTela();
 adicionarQuadrados();
